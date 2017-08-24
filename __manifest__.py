8c73d077490d47684f3f50127600666ec782c074
mhdsyarif <mhdsyarif.ms@gmail.com>
2017-08-24 18:37:43 +0700

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Company Experience',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'summary': 'Experience Project',
+    'author': 'Muhammad Syarif',
+    'email': 'mhdsyarif.ms@gmail.com',
+    'website': 'https://www.mhdsyarif.com',
+    'license': 'AGPL-3',
+    'depends': [],
+    'data': [
+        'views/rp_experience.xml',
+    ],
+    'images': ['static/description/syarif.png'],
+    'installable': True,
+    'auto_install': False
+}
---
+++ models/rp_experience.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class Experience(models.Model):
+    _name = 'company.experience'
+    _description = "Data Experience"
+
+    num_contract = fields.Char(string="Contract Number")
+    job_name = fields.Char(string="Job Name", required=True)
+    service_user = fields.Char(string="Service User", required=True)
+    scop_service = fields.Text(string="Scop of Service", required=True)
+    start_date = fields.Date(string="Start Date", default=fields.Date.to_string)
+    end_date = fields.Date(string="End Date",default=fields.Date.to_string)
+    price = fields.Float(string="Price", required=True)
+    status = fields.Selection(string="Status", selection=[('1','Ongoing'),('2','Pending'),('3','Finish')], required=True)
+    company_id = fields.Many2one('res.company',string="Company", required=True)
