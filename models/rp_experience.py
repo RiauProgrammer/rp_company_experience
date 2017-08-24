6c5d207e88be2706974d7d1ab8b8258b18e22eb4
mhdsyarif <mhdsyarif.ms@gmail.com>
2017-08-24 18:51:58 +0700

    Added field periode

--- models/rp_experience.py
+++ models/rp_experience.py
@@ -10,6 +10,7 @@ class Experience(models.Model):
     job_name = fields.Char(string="Job Name", required=True)
     service_user = fields.Char(string="Service User", required=True)
     scop_service = fields.Text(string="Scop of Service", required=True)
+    periode = fields.Char(string="Periode", required=True)
     start_date = fields.Date(string="Start Date", default=fields.Date.to_string)
     end_date = fields.Date(string="End Date",default=fields.Date.to_string)
     price = fields.Float(string="Price", required=True)
