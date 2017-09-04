0f6c9e5fcdd2a9dc5dc3a7eb7c257ad00b8a62a0
mhdsyarif <mhdsyarif.ms@gmail.com>
2017-09-04 18:24:05 +0700

    Added track_visibility for history field

--- models/rp_experience.py
+++ models/rp_experience.py
@@ -8,44 +8,54 @@ class Experience(models.Model):
     _inherit = ['mail.thread']
 
     num_contract = fields.Char(
-        string="Contract Number"
+        string="Contract Number", 
+        track_visibility='onchange'
     )
     job_name = fields.Char(
         string="Job Name", 
-        required=True
+        required=True, 
+        track_visibility='onchange'
     )
     partner_id = fields.Many2one('res.partner',
         string="Customer", 
-        required=True
+        required=True, 
+        track_visibility='onchange'
     )
     scope = fields.Text(
         string="Scop of Service", 
-        required=True
+        required=True, 
+        track_visibility='onchange'
     )
     periode = fields.Char(
         string="Periode", 
-        required=True
+        required=True, 
+        track_visibility='onchange'
     )
     start_date = fields.Date(
         string="Start Date", 
-        default=fields.Date.to_string
+        default=fields.Date.to_string, 
+        track_visibility='onchange'
     )
     end_date = fields.Date(
         string="End Date",
-        default=fields.Date.to_string
+        default=fields.Date.to_string, 
+        track_visibility='onchange'
     )
     price = fields.Float(
         string="Price", 
-        required=True
+        required=True, 
+        track_visibility='onchange'
     )
     status = fields.Selection(
         string="Status", 
         selection=[('1','Ongoing'),('2','Pending'),('3','Finish')], 
-        required=True
+        required=True, 
+        track_visibility='onchange'
     )
     company_id = fields.Many2one('res.company',
         string="Company", 
-        required=True
+        required=True, 
+        track_visibility='onchange'
     )
 
     color = fields.Integer()
